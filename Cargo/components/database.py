829e1cad9a9ecc9b8c7082e6ff74bd898b70fe90
d10658 <sergoiwj1@gmail.com>
2024-09-10 21:57:49 +0300

    docker ports change

--- Cargo/components/database.py
+++ Cargo/components/database.py
@@ -10,7 +10,7 @@ DATABASES = {
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST', 'db'),
+        'HOST': os.environ.get('DB_HOST', 'localhost'),
         'PORT': os.environ.get('DB_PORT', 5432),
         'OPTIONS': {
             # Нужно явно указать схемы, с которыми будет работать приложение.
