ae248fcc24e2f4b36b5ab136e7bf5652d518a48f
d10658 <sergoiwj1@gmail.com>
2024-10-13 10:03:12 +0300

    fixed additional fields weren't disabling

--- static/js/disabling-additional-fields.js
+++ static/js/disabling-additional-fields.js
@@ -5,7 +5,6 @@ var size_el = document.getElementById('id_size');
 places_count_el.onchange = function () {
     var is_multiple_places = places_count_el.value > 1;
 
-    console.log(weight_el.value);
     weight_el.value = is_multiple_places ? 1 : weight_el.value;
     weight_el.disabled = is_multiple_places;
     size_el.disabled = is_multiple_places;
