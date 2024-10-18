e118704bade7012719f4a745e3d4c20642e5532a
d10658 <sergoiwj1@gmail.com>
2024-10-18 16:21:58 +0300

    completed logic-adding for edit for tracker-codes and IN

--- static/js/disabling-additional-fields.js
+++ static/js/disabling-additional-fields.js
@@ -2,6 +2,12 @@ var places_count_el = document.getElementById("id_places_count");
 var weight_el = document.getElementById("id_weight");
 var size_el = document.getElementById('id_size');
 
+var is_multiple_places = places_count_el.value > 1;
+
+weight_el.value = is_multiple_places ? 1 : weight_el.value;
+weight_el.disabled = is_multiple_places;
+size_el.disabled = is_multiple_places;
+
 places_count_el.onchange = function () {
     var is_multiple_places = places_count_el.value > 1;
 
